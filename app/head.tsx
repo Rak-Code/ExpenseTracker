d1423c44b14f4e37a160f56eb87bd3feb3e727f8
Rakesh Gupta <rakeshgupta1361411@gmail.com>
2025-05-23 17:52:21 +0530

    favicon

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>Expense Tracker</title>
+      <meta name="description" content="Track and analyze your expenses" />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
